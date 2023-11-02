d94ed78d60971a5d5550efaef33d1304ec462d8a
Putrawin23181 <149153022+Putrawin23181@users.noreply.github.com>
2023-11-02 20:27:55 +0700

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -102,3 +102,64 @@ for _ in range(jumlah_pizza):
             hargacrstuk = 66364 #harga jika ukuran regular
         else:
             hargacrstuk = 107273 #harga jika ukuran large
+    
+    #fungsi if untuk harga crust Crown Crust beserta dengan harga per ukuran pizza
+    else:
+        hargacrust = 55455 #harga crust Crown Crust
+        if ukuran == 1:
+            hargacrstuk = 0 #harga jika ukuran personal
+        elif ukuran == 2:
+            hargacrstuk = 65455 #harga jika ukuran regular
+        else:
+            hargacrstuk = 104545 #harga jika ukuran large
+
+    #fungsi if untuk jika pelanggan ingin menambahkan Extra Cheese beserta harga per ukuran pizza
+    if xtracz == "Y" or "y":
+        print("Add Extra Cheese")
+        if ukuran == 1:
+            hargacz = 13636 #harga jika ukuran personal
+        elif ukuran == 2:
+            hargacz = 16364 #harga jika ukuran regular
+        else:
+            hargacz = 19091 #harga jika ukuran large
+    else:
+        hargacz = 0 #harga jika pelanggan tidak menambahkan Extra Cheese
+
+    #deklarasi perhitungan total harga
+    total_harga = hargacrust + hargacrstuk + hargacz
+
+    #memasukkan data pesanan ke dalam list
+    pesanan_pizza.append({
+        "Topping": nama_topping,
+        "Crust": nama_crust,
+        "Ukuran": nama_ukuran,
+        "Extra Cheese": xtracz,
+        "Total Harga": total_harga
+    })
+
+#menghitung total keseluruhan harga pizza dengan SUM
+total_harga_semua_pesanan = sum(pesanan["Total Harga"] for pesanan in pesanan_pizza)
+
+#print tampilan bill
+print("========================================================")
+print("-----------PIZZA HUT----------")
+
+#membuat kolom tabel
+tabel.field_names = ["Rincian Pesanan", "Harga"]
+tabel_harga.field_names = ["Total Harga"]
+
+#mengisi baris pada tabel
+for pesanan in pesanan_pizza:
+    rincian_pesanan = f"{pesanan['Ukuran']} {pesanan['Topping']} \n{pesanan['Crust']}"
+    if pesanan['Extra Cheese'].lower() == 'y':
+        rincian_pesanan += "\nAdd Extra Cheese"
+    harga_pesanan = "Rp. {:,.2f}".format(pesanan['Total Harga'])
+    tabel.add_row([rincian_pesanan, harga_pesanan])
+
+#mengisi baris pada tabel_harga
+tabel_harga.add_row(["Rp. {:,.2f}".format(total_harga_semua_pesanan)])
+
+#print tabel
+print(tabel)
+print(tabel_harga)
+print("Terima Kasih Atas Kunjungan Anda")
